84ff3108fa9b3ef3b7f7fd4d07d58c037e8dc5c2
Phanindra <phanindra915@gmail.com>
2026-07-16 07:10:44 +0000

    modified exercise 3- exec() failure

--- linux-system-programming/processes/exec-3.c
+++ linux-system-programming/processes/exec-3.c
@@ -6,23 +6,13 @@
 
 int main(){
 
-    // create a child process
-    pid_t pid = fork();
-
-    printf("Creating a child process...");
-    if(pid == 0){
-        //child process
-        //execute unknown program
-        char *args[] = {NULL};
-        execvp("unknown", args);
-        //if execvp() fails
-        perror("execvp");
-        _exit(EXIT_FAILURE);
-        printf("This is never printed");
-    }else{
-        wait(NULL); // reaps the child process
-        printf("\nChild process terminated!\n");
-    }
-
+    printf("Hello");
+    // executing unknown program
+    char *args[] = {NULL};
+    execvp("unknown", args);
+    // executes only if execvp returns failure
+    perror("execvp");
+    // no cleanup
+    _exit(EXIT_FAILURE);
     return 0;
 }
