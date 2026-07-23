f463e48a1dbdc1185c6744612d001f7ce7fac01a
Phanindra <phanindra915@gmail.com>
2026-07-23 05:25:48 +0000

    program to restart interrupted system calls after handling SIGINT

--- linux-system-programming/signals/sigaction/signal-2.c
+++ linux-system-programming/signals/sigaction/signal-2.c
@@ -1,1 +1,25 @@
-// this program 
+// this program uses modern signal API
+#include <stdio.h>
+#include <string.h> //strlen()
+#include <signal.h>
+#include <unistd.h> // write()
+
+void handler(int sig){
+    printf("Caught SIGINT\n");
+}
+
+int main(){
+
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sigaction(SIGINT, &sa, NULL);
+
+    sigemptyset(&sa.sa_mask); // doesn't block any other signals while handling current signal handler.
+    sa.sa_flags = SA_RESTART; // interrupted system call restarts by the kernel
+    
+    char buf[1024];
+    fgets(buf, sizeof buf, stdin);
+    
+    write(1, buf, strlen(buf));
+    return 0;
+}
